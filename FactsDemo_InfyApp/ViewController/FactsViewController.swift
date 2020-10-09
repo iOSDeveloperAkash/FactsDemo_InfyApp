e9ef6900b723977fced87ea010baaf5184d322ee
Akash SIngh <akash.singh260895@gmail.com>
2020-10-09 17:07:15 +0530

    tableview setup completed along with refresh control.

--- FactsDemo_InfyApp/ViewController/FactsViewController.swift
+++ FactsDemo_InfyApp/ViewController/FactsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FactsViewController.swift
 //  FactsDemo_InfyApp
 //
 //  Created by Akash on 09/10/20.
@@ -11,23 +11,41 @@ import UIKit
 class FactsViewController: UIViewController {
     
     let factsTableView = UITableView()
-//    var dataSource: FactsDataSource?
+    //    var dataSource: FactsDataSource?
     private let refreshControl = UIRefreshControl()
-//    private var viewModel: FactsViewModel?
-
+    //    private var viewModel: FactsViewModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         intialSetUp()
+        setupUIRefreshControl()
     }
     
-    func intialSetUp() {
-         view.backgroundColor = .orange
-//         self.setTableView()
-//         self.registerCells()
-//         self.viewModel?.fetchServiceCall()
-     }
-
-
+    fileprivate func intialSetUp() {
+        view.backgroundColor = .white
+        configureTableView()
+    }
+    
+    fileprivate func configureTableView(){
+        view.addSubview(factsTableView)
+        factsTableView.translatesAutoresizingMaskIntoConstraints = false
+        factsTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        factsTableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        factsTableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        factsTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+    fileprivate func setupUIRefreshControl() {
+        refreshControl.addTarget(self, action: #selector(serviceCall), for: UIControl.Event.valueChanged)
+        self.factsTableView.addSubview(refreshControl)
+    }
+    
+    @objc func serviceCall() {
+        DispatchQueue.main.async {
+//            self.viewModel?.fetchServiceCall()
+        }
+        refreshControl.endRefreshing()
+    }
 }
 
