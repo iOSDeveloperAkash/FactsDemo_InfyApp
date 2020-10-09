601c9a25041fc622a8fb0512a9adb683d27522a8
Akash SIngh <akash.singh260895@gmail.com>
2020-10-09 15:04:03 +0530

    Initial Commit

---
+++ FactsDemo_InfyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FactsDemo_InfyApp
+//
+//  Created by Akash on 09/10/20.
+//  Copyright © 2020 FactsDemo_InfyApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
